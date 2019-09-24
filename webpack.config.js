a85801fa3eb2ae9755013582becb4351af455c88
Jeff Ong <jefftimbre@outlook.com>
2019-09-24 13:06:36 -0400

    update
    Signed-off-by: Jeff Ong <jefftimbre@outlook.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,24 @@
- const path = require('path');
- module.exports = {
-     mode: 'development',
-     entry: './src/',
-     output: {
-         path: path.resolve(__dirname, './dist'),
-         filename: 'game.bundle.js'
-     },
-     module: {
-         rules: [{
-             test: /\.js$/,
-             loader: 'babel-loader',
-             query: {
-                 presets: ['env']
-             }
-         }]
-     },
-     stats: {
-         colors: true
-     },
-     devtool: 'source-map'
- };
+const path = require("path");
+module.exports = {
+  mode: "development",
+  entry: "./src/",
+  output: {
+    path: path.resolve(__dirname, "./public"),
+    filename: "game.bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        query: {
+          presets: ["env"]
+        }
+      }
+    ]
+  },
+  stats: {
+    colors: true
+  },
+  devtool: "source-map"
+};
